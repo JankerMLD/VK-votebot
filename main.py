0ff351f784d6a180a99b42674eaf2cd726a3c34d
JankerMLD <serebr.s2012@gmail.com>
2024-03-01 10:15:27 +0300

    Разобрался с проблемой импорта vk_api

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import random
-#import vk_api
-#import pandas
+import sys
 
+import vk_api
+#sys.path.append('C:/users/sereb/appdata/local/packages/pythonsoftwarefoundation.python.3.10_qbz5n2kfra8p0/localcache/local-packages/python38/site-packages')
 #from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+#print(sys.path)
