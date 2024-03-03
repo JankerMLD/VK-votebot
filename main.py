0ba7f0600a99de8d07892f024540f8c60f641f6c
JankerMLD <serebr.s2012@gmail.com>
2024-03-03 17:02:25 +0300

    Разбираюсь с багом

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import requests
 import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType 
 
-vk_session = vk_api.VkApi(token='vk1.a.l1-hDX_RdQh67WabeW')
-longpoll = VkBotLongPoll(vk_session, '224606869')
-vk = vk_session.get_api()
+#vk_session = vk_api.VkApi(token="vk1.a.l1-hDX_RdQh67WabeW")
+#longpoll = VkBotLongPoll(vk_session, "224606869")
+#vk = vk_session.get_api()
 
 
 # ключ vk1.a.l1-hDX_RdQh67WabeW id 224606869
