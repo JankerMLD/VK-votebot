b994dd37bb34ea467e95cfb7547cdba6a9bb58a7
JankerMLD <serebr.s2012@gmail.com>
2024-03-04 13:15:55 +0300

    AAAAAAAAAAA

--- Pet_Projects/VK_votebot/main.py
+++ Pet_Projects/VK_votebot/main.py
@@ -1,11 +1,12 @@
-import random
-import pickle
 import requests
 import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType 
 
-vk_session = vk_api.VkApi(token="vk1.a.l1-hDX_RdQh67WabeW")
-longpoll = VkBotLongPoll(vk_session, "224606869")
+TOKEN = "vk1.a.l1-hDX_RdQh67WabeW"
+ID = "224606869"
+
+vk_session = vk_api.VkApi(token=TOKEN)
+longpoll = VkBotLongPoll(vk_session, ID)
 vk = vk_session.get_api()
 
 
