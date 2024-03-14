5fe6febf68fce57bf41822aaa7dd20369dd4a163
JankerMLD <serebr.s2012@gmail.com>
2024-03-14 15:13:48 +0300

    Исправил ключ токена

--- Pet_Projects/VK_votebot/main.py
+++ Pet_Projects/VK_votebot/main.py
@@ -2,7 +2,7 @@ import requests
 import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType 
 
-TOKEN = "vk1.a.l1-hDX_RdQh67WabeW"
+TOKEN = "vk1.a.l1-hDX_RdQh67WabeW2GS0VPAU2j-lQ-G-L5jTOGGXas7QfNdCom81vxwbk1_jq-uISnHBdD9Z-VD-eFPxRP7W3h-IXM--KOdvJO6kntXWpr7nZ7hvtLB9pJLJVCvdd9iiqQjQ8OSlu_A4nWRfCObcNHRL6q1zyJtMAkdkJT5Ekxiq1pkfRsdEwiJCAi9VVuJC007wbLqbLTrYypk3eSJQ"
 ID = "224606869"
 
 vk_session = vk_api.VkApi(token=TOKEN)
@@ -10,4 +10,5 @@ longpoll = VkBotLongPoll(vk_session, ID)
 vk = vk_session.get_api()
 
 
-# ключ vk1.a.l1-hDX_RdQh67WabeW id 224606869
+# id 224606869
+# ключ vk1.a.l1-hDX_RdQh67WabeW2GS0VPAU2j-lQ-G-L5jTOGGXas7QfNdCom81vxwbk1_jq-uISnHBdD9Z-VD-eFPxRP7W3h-IXM--KOdvJO6kntXWpr7nZ7hvtLB9pJLJVCvdd9iiqQjQ8OSlu_A4nWRfCObcNHRL6q1zyJtMAkdkJT5Ekxiq1pkfRsdEwiJCAi9VVuJC007wbLqbLTrYypk3eSJQ
