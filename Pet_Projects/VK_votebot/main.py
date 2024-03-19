634da841fc5b5e51a417c15a9772ea73f8b4e5b8
JankerMLD <serebr.s2012@gmail.com>
2024-03-19 15:10:54 +0300

    readability improvement

--- Pet_Projects/VK_votebot/main.py
+++ Pet_Projects/VK_votebot/main.py
@@ -2,7 +2,8 @@ import requests
 import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType 
 
-TOKEN = "vk1.a.l1-hDX_RdQh67WabeW2GS0VPAU2j-lQ-G-L5jTOGGXas7QfNdCom81vxwbk1_jq-uISnHBdD9Z-VD-eFPxRP7W3h-IXM--KOdvJO6kntXWpr7nZ7hvtLB9pJLJVCvdd9iiqQjQ8OSlu_A4nWRfCObcNHRL6q1zyJtMAkdkJT5Ekxiq1pkfRsdEwiJCAi9VVuJC007wbLqbLTrYypk3eSJQ"
+TOKEN = '''vk1.a.l1-hDX_RdQh67WabeW2GS0VPAU2j-lQ-G-L5jTOGGXas7QfNdCom81vxwbk1_jq-uISnHBdD9Z-VD-eFPxRP7W3h-IXM--KOdvJO6kntXWpr7nZ7hvtLB9p
+JLJVCvdd9iiqQjQ8OSlu_A4nWRfCObcNHRL6q1zyJtMAkdkJT5Ekxiq1pkfRsdEwiJCAi9VVuJC007wbLqbLTrYypk3eSJQ'''
 ID = "224606869"
 
 vk_session = vk_api.VkApi(token=TOKEN)
@@ -11,4 +12,5 @@ vk = vk_session.get_api()
 
 
 # id 224606869
-# ключ vk1.a.l1-hDX_RdQh67WabeW2GS0VPAU2j-lQ-G-L5jTOGGXas7QfNdCom81vxwbk1_jq-uISnHBdD9Z-VD-eFPxRP7W3h-IXM--KOdvJO6kntXWpr7nZ7hvtLB9pJLJVCvdd9iiqQjQ8OSlu_A4nWRfCObcNHRL6q1zyJtMAkdkJT5Ekxiq1pkfRsdEwiJCAi9VVuJC007wbLqbLTrYypk3eSJQ
+# ключ vk1.a.l1-hDX_RdQh67WabeW2GS0VPAU2j-lQ-G-L5jTOGGXas7QfNdCom81vxwbk1_jq-uISnHBdD9Z-VD-eFPxRP7W3h-IXM--KOd
+# vJO6kntXWpr7nZ7hvtLB9pJLJVCvdd9iiqQjQ8OSlu_A4nWRfCObcNHRL6q1zyJtMAkdkJT5Ekxiq1pkfRsdEwiJCAi9VVuJC007wbLqbLTrYypk3eSJQ
