13badc64e0e6452af9c85f0848e802298f53dfa6
JankerMLD <serebr.s2012@gmail.com>
2024-03-21 13:02:58 +0300

    added bot  foundation

--- Pet_Projects/VK_votebot/main.py
+++ Pet_Projects/VK_votebot/main.py
@@ -1,3 +1,7 @@
+# id 224606869
+# ключ vk1.a.l1-hDX_RdQh67WabeW2GS0VPAU2j-lQ-G-L5jTOGGXas7QfNdCom81vxwbk1_jq-uISnHBdD9Z-VD-eFPxRP7W3h-IXM--KOd
+# vJO6kntXWpr7nZ7hvtLB9pJLJVCvdd9iiqQjQ8OSlu_A4nWRfCObcNHRL6q1zyJtMAkdkJT5Ekxiq1pkfRsdEwiJCAi9VVuJC007wbLqbLTrYypk3eSJQ
+
 import requests
 import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType 
@@ -10,7 +14,11 @@ vk_session = vk_api.VkApi(token=TOKEN)
 longpoll = VkBotLongPoll(vk_session, ID)
 vk = vk_session.get_api()
 
+def write_msg(user_id, message):
+    vk.method('messages.send', {'user_id': user_id, 'message': message})
 
-# id 224606869
-# ключ vk1.a.l1-hDX_RdQh67WabeW2GS0VPAU2j-lQ-G-L5jTOGGXas7QfNdCom81vxwbk1_jq-uISnHBdD9Z-VD-eFPxRP7W3h-IXM--KOd
-# vJO6kntXWpr7nZ7hvtLB9pJLJVCvdd9iiqQjQ8OSlu_A4nWRfCObcNHRL6q1zyJtMAkdkJT5Ekxiq1pkfRsdEwiJCAi9VVuJC007wbLqbLTrYypk3eSJQ
+for event in longpoll.listen():
+    if event.type == VkBotEventType.MESSAGE_NEW:
+        if event.to_me:
+            request = event.text
+            
